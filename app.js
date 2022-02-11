57f01451d4150c9429d37454a348d19cce6931a8
MadeleineWallner <madeleine.wallner@gmail.com>
2022-02-11 11:09:03 +0100

    update

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 });
 
 
-
-app.listen(3000, () => {
-    console.log("The application is running on localhost:3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`The application is running on port ${port}`)
 });
